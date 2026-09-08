bda40f325fc5740ef340355f00da89823529e3cb
Sunil Singh <singhsunil34500@gmail.com>
2026-09-08 12:47:00 +0530

    few changes in exixting code

--- mode_ltraining.py
+++ mode_ltraining.py
@@ -1,18 +1,19 @@
 import pymupdf
 import fitz
+import re     # 
 
 # how to convert text into Name identity form
-# import spacy
-# nlp = spacy.load("en_core_web_sm")
-# doc = nlp("Python is a programming language. ")
-# for token in doc:
-#     print(token.text, token.pos_)
+import spacy
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Python is a programming language. ")
+for token in doc:
+    print(token.text, token.pos_)
 
 
 # For pdf extraction we use fitz
-document=fitz.open("data/SUNIL_RESUME.pdf")
-text=""
-for page in document:
-     text += page.get_text()
-print(text) 
-document.close()
+# document=fitz.open("data/SUNIL_RESUME.pdf")
+# text=""
+# for page in document:
+#      text += page.get_text()
+# print(text) 
+# document.close()
--- src/hello.py
+++
@@ -1,8 +0,0 @@
-import fitz
-# For pdf extraction we use fitz
-document=fitz.open("data/SUNIL_RESUME.pdf")
-text=""
-for page in document:
-     text += page.get_text()
-print(text) 
-document.close()
